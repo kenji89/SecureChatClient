eddecc43d8c899d7304a0a76fca2ae256ec7afa8
kenji89 <mruczek89@gmail.com>
2015-04-02 21:06:04 +0200

    initial commit

---
+++ src/main/java/pl/marczykm/SecureChatClient/App.java
@@ -0,0 +1,13 @@
+package pl.marczykm.SecureChatClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
