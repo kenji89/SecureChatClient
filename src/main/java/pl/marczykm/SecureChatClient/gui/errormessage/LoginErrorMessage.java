2506e6b48fdb9f81391c22d776f9dceb7dffab74
kenji89 <mruczek89@gmail.com>
2015-04-04 18:00:24 +0200

    added fields validation, moved LoginView to separated class

---
+++ src/main/java/pl/marczykm/SecureChatClient/gui/errormessage/LoginErrorMessage.java
@@ -0,0 +1,6 @@
+package pl.marczykm.SecureChatClient.gui.errormessage;
+
+public class LoginErrorMessage {
+	public static String EMPTY_LOGIN_FIELD = "Login field is empty";
+	public static String EMPTY_PASSWORD_FIELD = "Password field is empty";
+}
