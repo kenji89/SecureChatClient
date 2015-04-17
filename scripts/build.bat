b57c742668b2c168e27e7219f6403c128f7fc89a
kenji89 <mruczek89@gmail.com>
2015-04-17 20:50:06 +0200

    added better building script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,7 @@
 cd ..
-mvn clean install
+mvn clean install > logs\build.log
+cd target
+rm SecureChatClient-0.0.1-SNAPSHOT.jar
+cp SecureChatClient-0.0.1-SNAPSHOT-jar-with-dependencies.jar SecureChatClient-0.0.1-SNAPSHOT.jar
+rm SecureChatClient-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+pause
